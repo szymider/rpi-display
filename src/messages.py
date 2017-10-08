d045ef9fc29a41d532a77482d495c9949fb84fc8
Adam Klimko <exusar@gmail.com>
2017-10-09 00:25:52 +0200

    Renamed functions to make them private

--- src/messages.py
+++ src/messages.py
@@ -4,23 +4,23 @@ import time
 from collections import deque
 
 
-def get_messages_url(resource):
+def _get_messages_url(resource):
     return "http://api.adamklimko.pl/raspberry/messages/{}".format(resource)
 
 
-def get_last_id():
-    response = requests.get(url=get_messages_url('read'))
+def _get_last_id():
+    response = requests.get(url=_get_messages_url('read'))
     data = response.json()
     return int(data['lastReadId'])
 
 
-def get_new_messages():
-    return requests.get(url=get_messages_url('from/{}'.format(last_received_id))).json()
+def _get_new_messages():
+    return requests.get(url=_get_messages_url('from/{}'.format(last_received_id))).json()
 
 
 def load_messages():
     global messages_to_read, last_received_id
-    new_messages = get_new_messages()
+    new_messages = _get_new_messages()
     if new_messages:
         messages_to_read.extend(new_messages)
         last_received_id = new_messages[-1]['id']
@@ -28,9 +28,9 @@ def load_messages():
 
 
 def set_read_id(read):
-    requests.put(url=get_messages_url('read'), headers={'Content-Type': 'application/json'},
+    requests.put(url=_get_messages_url('read'), headers={'Content-Type': 'application/json'},
                  json={'lastReadId': read})
 
 
-last_received_id = get_last_id()
+last_received_id = _get_last_id()
 messages_to_read = deque()
