0fabf3863fc075998be6525f32454faf1b9facb7
Adam Klimko <exusar@gmail.com>
2017-10-11 19:52:52 +0200

    Extracted variables from urls

--- src/constants.py
+++ src/constants.py
@@ -13,7 +13,8 @@ DISPLAY_RATE_DATE = 5
 # Weather
 DISPLAY_RATE_WEATHER = 5
 UPDATE_RATE_WEATHER = 300
-URL_WEATHER = "http://api.wunderground.com/api/ec90855c0d358c5f/conditions/q/pws:IKRAKOW17.json"
+WEATHER_API_KEY = 'ec90855c0d358c5f'
+URL_WEATHER = "http://api.wunderground.com/api/{}/conditions/q/pws:IKRAKOW17.json".format(WEATHER_API_KEY)
 
 # Currency
 DISPLAY_RATE_CURRENCY = 3
@@ -24,7 +25,8 @@ URL_USD = "http://api.nbp.pl/api/exchangerates/rates/a/usd/?format=json"
 # Instagram followers
 DISPLAY_RATE_IG = 5
 UPDATE_RATE_IG = 360
-URL_IG = "https://www.instagram.com/aredos/?__a=1"
+IG_USERNAME = 'aredos'
+URL_IG = "https://www.instagram.com/{}/?__a=1".format(IG_USERNAME)
 
 # Dead period of time after clicking a button
 WAIT_TIME_AFTER_CLICK = 0.4
@@ -32,9 +34,9 @@ WAIT_TIME_AFTER_CLICK = 0.4
 # Brightness (hour : brightness)
 HOURS = {
     7: 1,
-    11: 3,
-    15: 5,
-    18: 3,
+    11: 2,
+    15: 3,
+    18: 2,
     24: 1
 }
 HOURS = OrderedDict(sorted(HOURS.items(), key=lambda item: item[0]))
