1afb54201f1f8cc7854dede33c70b454823cb088
Adam Klimko <exusar@gmail.com>
2018-10-07 18:14:54 +0200

    Change buttons gpio numbers access

--- src/buttons.py
+++ src/buttons.py
@@ -8,8 +8,8 @@ import configuration
 
 class Buttons:
     def __init__(self, device, mode):
-        self.left = 17
-        self.right = 26
+        self._left = 17
+        self._right = 26
         self._setup_gpio()
         self._buttons_cfg = configuration.ButtonsCfg()
         self._device = device
@@ -17,20 +17,20 @@ class Buttons:
 
     def _setup_gpio(self):
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.left, GPIO.IN)
-        GPIO.setup(self.right, GPIO.IN)
-        GPIO.add_event_detect(self.left, GPIO.RISING, bouncetime=250)
-        GPIO.add_event_detect(self.right, GPIO.RISING, bouncetime=250)
+        GPIO.setup(self._left, GPIO.IN)
+        GPIO.setup(self._right, GPIO.IN)
+        GPIO.add_event_detect(self._left, GPIO.RISING, bouncetime=250)
+        GPIO.add_event_detect(self._right, GPIO.RISING, bouncetime=250)
 
     def setup_listener(self):
         threading.Thread(target=self._listener, daemon=True).start()
 
     def _listener(self):
         while True:
-            if GPIO.event_detected(self.left):
+            if GPIO.event_detected(self._left):
 
                 time.sleep(self._buttons_cfg.get_wait_time_after_click())
-            elif GPIO.event_detected(self.right):
+            elif GPIO.event_detected(self._right):
                 self._mode.switch()
                 self._device.clear()
                 time.sleep(self._buttons_cfg.get_wait_time_after_click())
