500949a06a5575140b95cb69a6a55d7dabcd53f2
szymider <73285988+szymider@users.noreply.github.com>
2020-11-17 17:54:01 +0100

    Update weather_providers.py

--- rpidisplay/weather_providers.py
+++ rpidisplay/weather_providers.py
@@ -7,7 +7,7 @@ from rpidisplay import configuration
 
 class OpenWeatherMap:
     def __init__(self):
-        self._url = 'http://api.openweathermap.org/data/2.5/weather?id=New York&appid=5c4014c79199970216bf8aac570511dd'
+        self._url = 'http://api.openweathermap.org/data/2.5/weather?iq=NewYork&appid=5c4014c79199970216bf8aac570511dd'
         self._weather_cfg = configuration.WeatherCfg()
 
     def download_data(self):
@@ -34,31 +34,3 @@ class OpenWeatherMap:
         }
 
 
-class DarkSky:
-    def __init__(self):
-        self._url = 'https://api.darksky.net/forecast/{}/{}/?units={}&exclude=[minutely,hourly,daily,alerts,flags]'
-        self._weather_cfg = configuration.WeatherCfg()
-
-    def download_data(self):
-        units = {
-            'C': 'si',
-            'F': 'us',
-        }
-        unit = units[self._weather_cfg.get_unit().upper()]
-
-        response = requests.get(
-            self._url.format(self._weather_cfg.get_api_key(), self._weather_cfg.get_location(), unit))
-
-        status_code = response.status_code
-        if status_code / 100 != 2:
-            logging.error('Cannot download weather using provider=DarkSky, status code=%d', status_code)
-            return
-
-        json = response.json()
-
-        return {
-            'unit': self._weather_cfg.get_unit().upper(),
-            'temp': int(json['currently']['temperature']),
-            'pressure': int(json['currently']['pressure']),
-            'humidity': json['currently']['humidity'] * 100,
-        }
